19f47619f4159abcfa15d699835cad1ec5c08d00
Louise Grillon <louise@quiz-room.com>
2023-03-06 15:36:06 +0100

    fix last commit

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ function App() {
   return (
     <div className="App">
       <div className="header">Time for you</div>
-      <div className="rubrique"></div>
-      <div className="rubrique"></div>
-      <div className="rubrique"></div>
-      <div className="rubrique"></div>
+      <div className="rubrique">test</div>
+      <div className="rubrique">coucou</div>
+      <div className="rubrique">relax</div>
+      <div className="rubrique">yes</div>
     </div>
   );
 }
